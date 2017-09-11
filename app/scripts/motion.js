b9c7503938de2fb869c7da53429b6c29d0a95158
sasaki.mariko <mariko.sasaki@dena.com>
2017-09-11 19:04:32 +0900

    会社でちょっと修正

--- app/scripts/motion.js
+++ app/scripts/motion.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	$(".main").css("height", hsize + "px");	
 
-	$('.effect div, .effect i ,header ,.main-massage').css("opacity","0");
+	$('.effect>.container, .effect i ,header ,.main-massage').css("opacity","0");
 
 	TweenMax.to('.main-massage', 2, {
 		opacity: 1,
@@ -25,20 +25,19 @@ document.addEventListener('DOMContentLoaded', function() {
 			TweenMax.to('header', 1, {opacity: 0});
 		}
 
-
 		$(".effect").each(function(){
 			var imgPos = $(this).offset().top;
 			var scroll = $(window).scrollTop();
 			var windowHeight = $(window).height();
 
 			if (scroll > imgPos - windowHeight + windowHeight/5){
-				$("h2, div",this).css("opacity","1" );
+				$("h2 ,.container",this).css("opacity","1" );
 				$("i",this).css({
 				 // "font-size": "100px",
 				 // "padding": "0 20px 40px"
 				});
 			} else {
-				$("h2, div",this).css("opacity","0" );
+				$("h2 ,.container",this).css("opacity","0" );
 				$("i",this).css({
 				 // "font-size": "20px",
 				//  "padding": "20px"
