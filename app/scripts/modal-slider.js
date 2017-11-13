57afb19ee5a3be6af8c07b1958097bdc4c88a6a1
sasaki.mariko <mariko.sasaki@dena.com>
2017-11-13 19:23:27 +0900

    会社作業ぶん

--- app/scripts/modal-slider.js
+++ app/scripts/modal-slider.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	$(function () {
 		$("#navi a").click(function(){
 			 //$("#main img").attr("src" , $(this).attr("href"));
-			$("#main img").before("<img src='"+$(this).attr("href")+"'alt=''>");
+			$("#main img").before("<img src='"+$(this).attr("href")+"' width='500px' alt=''>");
 			//$("#main img").before("<img src='aaa'alt=''>");
 			$("#main img:last").fadeOut("fast" ,function(){
 				$(this).remove();
