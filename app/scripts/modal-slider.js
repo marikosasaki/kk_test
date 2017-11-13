4d35814e82ddb51817255f36cdb129db01675d0a
sasaki.mariko <mariko.sasaki@dena.com>
2017-11-13 17:36:53 +0900

    会社作業ぶん　css変更　スライドjs実装

---
+++ app/scripts/modal-slider.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+	$(function () {
+		$("#navi a").click(function(){
+			 //$("#main img").attr("src" , $(this).attr("href"));
+			$("#main img").before("<img src='"+$(this).attr("href")+"'alt=''>");
+			//$("#main img").before("<img src='aaa'alt=''>");
+			$("#main img:last").fadeOut("fast" ,function(){
+				$(this).remove();
+			})
+			return false;
+
+		})
+		// body...
+	})
+
+});
+
+
+
